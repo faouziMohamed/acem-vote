05910f2f2ef7bd653d8a4f9669d480a4f71e4c50
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-11-26 19:26:26 +0100

    Add Self Xss warning message

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,23 @@
 import '../sass/global.scss';
 
-// export function reportWebVitals(metric) {
-//   // eslint-disable-next-line no-console
-//   console.log(metric);
-// }
+import { useEffect } from 'react';
+
+// import { xssWarningMessage } from '../lib/utils/lib.utils';
+import noSelfXssWarning from '../lib/utils/no-self-xss';
+
+export function reportWebVitals(metric) {
+  // eslint-disable-next-line no-console
+  if (process.env.NODE_ENV !== 'production') console.log(metric);
+}
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    // eslint-disable-next-line no-console
+    console.clear();
+    const { cleanUp, printWarn } = noSelfXssWarning({ onLoad: false });
+    printWarn();
+    return cleanUp();
+  }, []);
   return <Component {...pageProps} />;
 }
 
